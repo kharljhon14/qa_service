85b2d69480be23fd98ddbae3479443882f2d3cf7
kharle14 <enriquezkharl14@gmail.com>
2024-03-27 00:06:40 +0800

    Added serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
-use serde::Serialize;
-use std::io::{Error, ErrorKind};
-use std::str::FromStr;
+use serde::{Deserialize, Serialize};
 use warp::{
-    filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
-    Rejection, Reply,
+    filters::cors::CorsForbidden,
+    http::{Method, StatusCode},
+    Filter, Rejection, Reply,
 };
 
-#[derive(Debug, Serialize)]
-struct QuestionId(String);
+use std::collections::HashMap;
 
-impl FromStr for QuestionId {
-    type Err = std::io::Error;
-
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        match id.is_empty() {
-            false => Ok(QuestionId(id.to_string())),
-            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
-        }
-    }
+#[derive(Clone)]
+struct Store {
+    questions: HashMap<QuestionId, Question>,
 }
 
-#[derive(Debug)]
-struct InvalidId;
+impl Store {
+    fn new() -> Self {
+        Store {
+            questions: Self::init(),
+        }
+    }
 
-impl Reject for InvalidId {}
+    fn init() -> HashMap<QuestionId, Question> {
+        let file = include_str!("../questions.json");
+        serde_json::from_str(file).expect("can't read questions.json")
+    }
 
-#[derive(Debug, Serialize)]
+    // fn add_question(mut self, question: Question) -> Self {
+    //     self.questions.insert(question.id.clone(), question);
+
+    //     self
+    // }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+struct QuestionId(String);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -33,29 +42,14 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-impl Question {
-    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
-        Question {
-            id,
-            title,
-            content,
-            tags,
-        }
-    }
-}
+async fn get_questions(
+    params: HashMap<String, String>,
+    store: Store,
+) -> Result<impl Reply, Rejection> {
+    println!("{:?}", params);
+    let res = store.questions.values().cloned().collect::<Vec<Question>>();
 
-async fn get_questions() -> Result<impl Reply, Rejection> {
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First Question".to_string(),
-        "Content of the question".to_string(),
-        Some(vec!["Faq".to_string()]),
-    );
-
-    match question.id.0.parse::<i32>() {
-        Err(_) => Err(warp::reject::custom(InvalidId)),
-        Ok(_) => Ok(warp::reply::json(&question)),
-    }
+    Ok(warp::reply::json(&res))
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
@@ -64,21 +58,26 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
             error.to_string(),
             StatusCode::FORBIDDEN,
         ))
-    } else if let Some(InvalidId) = r.find() {
-        Ok(warp::reply::with_status(
-            "No valid Id presented".to_string(),
-            StatusCode::UNPROCESSABLE_ENTITY,
-        ))
     } else {
         Ok(warp::reply::with_status(
             "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
+
+    // else if let Some(InvalidId) = r.find() {
+    //     Ok(warp::reply::with_status(
+    //         "No valid Id presented".to_string(),
+    //         StatusCode::UNPROCESSABLE_ENTITY,
+    //     ))
+    // }
 }
 
 #[tokio::main]
 async fn main() {
+    let store = Store::new();
+    let store_filter = warp::any().map(move || store.clone());
+
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
@@ -88,6 +87,8 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
+        .and(warp::query())
+        .and(store_filter)
         .and_then(get_questions)
         .recover(return_error);
 
